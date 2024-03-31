bf6535f3f2c3428c5b35062221372a3433316d50
Prudhvi-Nath-Reddy <mymail.prudhvi@gmail.com>
2024-04-01 02:36:29 +0530

    Homescreen hardcoded

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,279 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WeatherHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 20;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class WeatherHomePage extends StatelessWidget {
+  const WeatherHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: const SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hi devs:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            // This would be your current weather widget
+            CurrentLocDate(),
+            CurrentWeatherSection(),
+            DailyTips(),
+            // This would be your weekly forecast list
+            Expanded(child: WeeklyForecastList()),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Locations',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CurrentLocDate extends StatelessWidget {
+  const CurrentLocDate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Icon(Icons.location_on, color: Colors.black), // Location icon
+          SizedBox(width: 8), // Add space between the icon and the text
+          Text(
+            'Bangalore',
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge
+                ?.copyWith(color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class CurrentWeatherSection extends StatelessWidget {
+  const CurrentWeatherSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20.0),
+          bottomRight: Radius.circular(20.0),
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        // border: Border.all(
+        //   color: Colors.white, // Color of the border
+        //   width: 8.0, // Width of the border
+        // ),
+      ),
+      child: Column(
+        children: [
+          Text(
+            'March 12, 2024, 11:30 AM',
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white70),
+          ),
+          SizedBox(height: 20),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.energy_savings_leaf, color: Colors.white),
+                SizedBox(width: 5),
+                Text(
+                  'Comfort level: Moderate',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white),
+                ),
+                SizedBox(width: 10),
+                Icon(Icons.water_drop, color: Colors.white),
+                SizedBox(width: 5),
+                Text(
+                  'Humidity: 28%',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Icon(Icons.thermostat, color: Colors.white,size: 100,),
+              SizedBox(height: 10),
+              Text(
+                '27',
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white,fontSize: 70),
+
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+class DailyTips extends StatelessWidget {
+  const DailyTips({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // This would be where your list of weekly forecasts goes
+    return Container(
+      margin: const EdgeInsets.only(left: 16.0, right: 16.0,top: 16.0,bottom: 16.0),
+      padding: const EdgeInsets.all(16.0),
+      decoration:BoxDecoration(
+
+        color: Colors.white54,
+        border: Border.all(
+          width: 2 ,
+          color: Colors.black ,
+        ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20.0),
+          bottomRight: Radius.circular(20.0),
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Todays Temperature",
+              ),
+
+
+            ],
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Almost as same as Yesterday",
+              ),
+
+            ],
+          ),
+
+        ],
+      ),
+
+    );
+  }
+}
+
+class WeeklyForecastList extends StatelessWidget {
+  const WeeklyForecastList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // This would be where your list of weekly forecasts goes
+    return Container(
+      margin: const EdgeInsets.only(left: 16.0, right: 16.0,top: 16.0,bottom: 32),
+      padding: const EdgeInsets.all(16.0),
+      decoration:BoxDecoration(
+
+        color: Colors.white54,
+        border: Border.all(
+          width: 2 ,
+          color: Colors.black ,
+        ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20.0),
+          bottomRight: Radius.circular(20.0),
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Yesterday",
+              ),
+              Text("Today"),
+              Text("Monday"),
+              Text("Tuesday"),
+              Text("Wednesday"),
+              Text("Thursday"),
+              Text("Friday"),
+
+            ],
+          ),
+          SizedBox(width: 20,),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Moderate",
+              ),
+              Text("Moderate"),
+              Text("Moderate"),
+              Text("Moderate"),
+              Text("Moderate"),
+              Text("Low"),
+
+              Text("Moderate"),
+            ],
+          ),
+
+        ],
+      ),
+
     );
   }
 }
