e05cb37d391b280d4d6b87a925b7fd86b3f0d9b5
MuraliJayam27 <Sai.Muralikrishna@iiitb.ac.in>
2024-04-19 15:22:55 +0530

    Location page updated

--- lib/Location.dart
+++ lib/Location.dart
@@ -8,8 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Blank Flutter App',
-      debugShowCheckedModeBanner: false, // This line removes the debug tag
+      debugShowCheckedModeBanner: false, // Remove the debug tag
+      title: 'Location Screen',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -23,38 +23,64 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+        title: Text(
+          'Favorite Locations',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10),
+                LocationCard(
+                  locationName: 'Favorite Location 1',
+                  humidityLevel: 'Humidity: 70%',
+                ),
+                LocationCard(
+                  locationName: 'Favorite Location 2',
+                  humidityLevel: 'Humidity: 65%',
+                ),
+              ],
             ),
-            Text('Back',  style: TextStyle(fontSize: 18) ),
-          ],
-        ),
+          ),
+          Center(
+            child: SizedBox(
+              width: 200,
+              height: 42,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your manage location button logic here
+                  print('Manage Location button pressed');
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8), // Adjust the radius value here
+                  ),
+                ),
+                child: Text(
+                  'Manage Locations',
+                  style: TextStyle(
+                    color: Color(0xFF1F1F1F),
+                    fontSize: 14,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    height: 0.10,
+                    letterSpacing: 0.14,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-
-
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Add your button press logic here
-            print('Button pressed');
-          },
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          ),
-          child: Text('Manage locations', style: TextStyle(fontSize: 18),
-
-          ),
-        ),
-      ),
-
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.black87,
         unselectedItemColor: Colors.grey,
@@ -72,6 +98,29 @@ class MyHomePage extends StatelessWidget {
             label: 'Locations',
           ),
         ],
+      ),
+    );
+  }
+}
+
+class LocationCard extends StatelessWidget {
+  final String locationName;
+  final String humidityLevel;
+
+  LocationCard({
+    required this.locationName,
+    required this.humidityLevel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      margin: EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        leading: Icon(Icons.location_on),
+        title: Text(locationName),
+        subtitle: Text(humidityLevel),
       ),
     );
   }
