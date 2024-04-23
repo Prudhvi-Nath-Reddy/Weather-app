409d9b40bf3cb385c8c09e14f6195622a994ec22
Prudhvi-Nath-Reddy <mymail.prudhvi@gmail.com>
2024-04-23 15:27:59 +0530

    Map display failed

--- lib/mappage.dart
+++ lib/mappage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_map/flutter_map.dart';
 // import 'package:lat-long/lat-long.dart';
-
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
 class Map extends StatelessWidget {
   const Map({super.key});
 
@@ -9,35 +10,44 @@ class Map extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            Text('Back', style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
       ),
-      body: Center(
-      ),
+      body: const HelpScreen(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Add your search functionality here
           print('Search button pressed');
         },
-
         child: Icon(Icons.search),
         backgroundColor: Colors.white, // Customize button color if needed
         shape: CircleBorder(),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
 
+    );
+  }
+}
 
+class HelpScreen extends StatefulWidget {
+  const HelpScreen({super.key});
 
+  @override
+  State<HelpScreen> createState() => _HelpScreenState();
+}
+
+class _HelpScreenState extends State<HelpScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController();
+    _controller.loadFlutterAsset('lib/map1.html');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
