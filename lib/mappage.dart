d5c0c4eeef278b66ac7b043d786bc8c8780fb53a
harsha-deep <harshadeep.donapati@iiitb.org>
2024-05-17 17:10:55 +0530

    Added places autcomplete feature

--- lib/mappage.dart
+++ lib/mappage.dart
@@ -153,10 +153,159 @@
 // }
 
 // BACKUP CODE
+// import 'package:app1/constant.dart';
+// import 'package:flutter/material.dart';
+// import 'package:geocoding/geocoding.dart' as geocoding;
+// import 'package:geolocator/geolocator.dart';
+// import 'package:google_place/google_place.dart' as google_place;
+// import 'package:webview_flutter/webview_flutter.dart';
+// import 'webview.dart';
+
+// class MapScreen extends StatefulWidget {
+//   const MapScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _MapScreenState createState() => _MapScreenState();
+// }
+
+// class _MapScreenState extends State<MapScreen> {
+//   TextEditingController _searchController = TextEditingController();
+//   List<String> _suggestions = []; // List to hold the suggestions
+//   bool _isSearchFocused = false;
+
+//   late google_place.GooglePlace googlePlace;
+//   List<google_place.AutocompletePrediction> predictions = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     String apiKey = PLACES_API_KEY;
+//     googlePlace = google_place.GooglePlace(apiKey);
+//     _searchController.addListener(_onSearchChanged);
+//   }
+
+//   void autoCompleteSearch(String value) async {
+//     var result = await googlePlace.autocomplete.get(value);
+//     if (result != null && result.predictions != null && mounted) {
+//       setState(() {
+//         predictions = result.predictions!;
+//       });
+//     }
+//   }
+
+//   @override
+//   void dispose() {
+//     _searchController.dispose();
+//     super.dispose();
+//   }
+
+//   void _onSearchChanged() {
+//     String searchText = _searchController.text;
+//     if (searchText.length >= 1) {
+//       setState(() {
+//         autoCompleteSearch(searchText);
+//       });
+//     } else {
+//       setState(() {
+//         _suggestions.clear();
+//       });
+//     }
+//   }
+
+//   void _onSubmitted(String value) async {
+//     print('Search text: $value');
+//     List<geocoding.Location> locations =
+//         await geocoding.locationFromAddress(value);
+//     for (var location in locations) {
+//       print('Latitude: ${location.latitude}, Longitude: ${location.longitude}');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Map'),
+//       ),
+//       body: Column(
+//         children: [
+//           // Animated search bar
+//           AnimatedContainer(
+//             duration: Duration(milliseconds: 300),
+//             height: _isSearchFocused ? 80.0 : 0.0,
+//             child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: TextField(
+//                 controller: _searchController,
+//                 decoration: InputDecoration(
+//                   hintText: 'Search...',
+//                   border: OutlineInputBorder(),
+//                   contentPadding:
+//                       EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+//                 ),
+//                 style: TextStyle(fontSize: 18.0),
+//                 onChanged: (value) {
+//                   // Handle search text changes
+//                   if (value.isEmpty) {
+//                     setState(() {
+//                       _suggestions.clear();
+//                     });
+//                   }
+//                 },
+//                 onSubmitted: _onSubmitted, // Add onSubmitted callback
+//               ),
+//             ),
+//           ),
+//           // Suggestions list
+//           Visibility(
+//             visible: _isSearchFocused && _suggestions.isNotEmpty,
+//             child: Expanded(
+//               child: ListView.builder(
+//                 itemCount: _suggestions.length,
+//                 itemBuilder: (context, index) {
+//                   return ListTile(
+//                     title: Text(_suggestions[index]),
+//                     onTap: () {
+//                       // Handle when a suggestion is tapped
+//                       print('Selected suggestion: ${_suggestions[index]}');
+//                     },
+//                   );
+//                 },
+//               ),
+//             ),
+//           ),
+//           // WebView
+//           Expanded(
+//             child: MyWebView(
+//               selectedUrl:
+//                   "https://harsha-deep.github.io/projects/gee-humidity/map1.html",
+//             ),
+//           ),
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           setState(() {
+//             _isSearchFocused = !_isSearchFocused;
+//             if (!_isSearchFocused) {
+//               _searchController.clear();
+//               _suggestions.clear();
+//             }
+//           });
+//         },
+//         child: _isSearchFocused ? Icon(Icons.close) : Icon(Icons.search),
+//         backgroundColor: Colors.blue,
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+//     );
+//   }
+// }
+
+import 'dart:convert';
 import 'package:app1/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:geocoding/geocoding.dart' as geocoding;
-import 'package:geolocator/geolocator.dart';
 import 'package:google_place/google_place.dart' as google_place;
 import 'package:webview_flutter/webview_flutter.dart';
 import 'webview.dart';
@@ -181,7 +330,7 @@ class _MapScreenState extends State<MapScreen> {
     super.initState();
     String apiKey = PLACES_API_KEY;
     googlePlace = google_place.GooglePlace(apiKey);
-    _searchController.addListener(_onSearchChanged);
+    // _searchController.addListener(_onSearchChanged);
   }
 
   void autoCompleteSearch(String value) async {
@@ -193,18 +342,32 @@ class _MapScreenState extends State<MapScreen> {
     }
   }
 
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
+  Future<void> _fetchAutoComplete(String searchTerm) async {
+    String apiUrl =
+        'https://api.locationiq.com/v1/autocomplete?key=pk.e9c26940466d145644091d82289a570d&q=$searchTerm&limit=5';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        List<String> suggestions = [];
+        for (var item in jsonData) {
+          suggestions.add(item['display_place']);
+        }
+        setState(() {
+          _suggestions = suggestions;
+        });
+      } else {
+        throw Exception('Failed to load suggestions');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
   }
 
-  void _onSearchChanged() {
-    String searchText = _searchController.text;
-    if (searchText.length >= 1) {
-      setState(() {
-        autoCompleteSearch(searchText);
-      });
+  void _onSearchChanged(String value) {
+    if (value.length >= 1) {
+      _fetchAutoComplete(value);
     } else {
       setState(() {
         _suggestions.clear();
@@ -244,14 +407,7 @@ class _MapScreenState extends State<MapScreen> {
                       EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                 ),
                 style: TextStyle(fontSize: 18.0),
-                onChanged: (value) {
-                  // Handle search text changes
-                  if (value.isEmpty) {
-                    setState(() {
-                      _suggestions.clear();
-                    });
-                  }
-                },
+                onChanged: _onSearchChanged,
                 onSubmitted: _onSubmitted, // Add onSubmitted callback
               ),
             ),
@@ -266,8 +422,10 @@ class _MapScreenState extends State<MapScreen> {
                   return ListTile(
                     title: Text(_suggestions[index]),
                     onTap: () {
-                      // Handle when a suggestion is tapped
-                      print('Selected suggestion: ${_suggestions[index]}');
+                      setState(() {
+                        _searchController.text = _suggestions[index];
+                        _suggestions.clear();
+                      });
                     },
                   );
                 },
